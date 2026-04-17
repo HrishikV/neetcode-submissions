3435ba14a2ef87ce3fb3e284e12e343f0c6999e8
HrishikV <65384819+HrishikV@users.noreply.github.com>
2026-04-17 22:15:12 +0530

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # if len(s)%2!=0:
+        #     return False
+        start_p=['[','{','(']
+        proc=[]
+        pair={
+                '}':'{',
+                ']':'[',
+                ')':'('     }
+        for i in s:
+            if i in start_p:
+                proc.append(i)
+            elif not proc:
+                 return False
+            elif pair[i]!=proc.pop():
+                return False
+
+        return len(proc)==0
